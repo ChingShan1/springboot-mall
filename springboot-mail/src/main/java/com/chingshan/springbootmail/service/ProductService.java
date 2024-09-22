b47a77d224c4b33bb5e198b724b2c8151131dcc1
ChingShanHong <smmith818@gmail.com>
2024-09-22 20:44:51 +0800

    商品功能-實作查詢商品列表(查詢條件)

--- springboot-mail/src/main/java/com/chingshan/springbootmail/service/ProductService.java
+++ springboot-mail/src/main/java/com/chingshan/springbootmail/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.chingshan.springbootmail.service;
 
+import com.chingshan.springbootmail.constant.ProductCategory;
 import com.chingshan.springbootmail.dto.ProductRequest;
 import com.chingshan.springbootmail.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 

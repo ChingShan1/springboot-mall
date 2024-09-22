320c34decbe14dd4a0319b24f2ebc758ab40069a
ChingShanHong <smmith818@gmail.com>
2024-09-22 10:51:36 +0800

    商品功能-實作查詢商品

--- springboot-mail/src/main/java/com/chingshan/springbootmail/dao/impl/ProductDaoImpl.java
+++ springboot-mail/src/main/java/com/chingshan/springbootmail/dao/impl/ProductDaoImpl.java
@@ -15,20 +15,23 @@ import java.util.Map;
 public class ProductDaoImpl implements ProductDao {
 
     @Autowired
-    private NamedParameterJdbcTemplate NamedParameterJdbcTemplate;
-    @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public Product getProductById(Integer productId) {
-        String sql = "SELECT product_id,product_name, category, image_url, price, stock, description, " +
-                "created_date, last_modified_date " +
-                "FROM product WHERE product_id = :productID";
+        String sql = "SELECT product_id,product_name ,category, image_url, price, stock, description, created_date, last_modified_date FROM product WHERE product_id = :productId";
 
         Map<String, Object> map = new HashMap<>();
         map.put("productId", productId);
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
-        return null;
+
+        if(productList.size() > 0 ){
+            return productList.get(0);
+        }else{
+            return null;
+        }
+
+
     }
 }

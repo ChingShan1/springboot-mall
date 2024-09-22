f4d5c0d054514afebd49d8a5caa4e726c5bfd326
ChingShanHong <smmith818@gmail.com>
2024-09-22 11:16:51 +0800

    商品功能-Refactor category , use enum function

--- springboot-mail/src/main/java/com/chingshan/springbootmail/rowmapper/ProductRowMapper.java
+++ springboot-mail/src/main/java/com/chingshan/springbootmail/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.chingshan.springbootmail.rowmapper;
 
+import com.chingshan.springbootmail.constant.ProductCategory;
 import com.chingshan.springbootmail.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,13 @@ public class ProductRowMapper implements RowMapper<Product>{
 
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+
+        String categoryStr = rs.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+//        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
+
         product.setImageURL(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));

acc7ce8e6d87b6e789c9341d35038b3d1a268d9e
ChingShanHong <smmith818@gmail.com>
2024-09-23 10:24:51 +0800

    商品功能-改善參數傳遞 (排序)

--- springboot-mail/src/main/java/com/chingshan/springbootmail/controller/ProductController.java
+++ springboot-mail/src/main/java/com/chingshan/springbootmail/controller/ProductController.java
@@ -21,12 +21,20 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 filter
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy, // 如果沒有傳參數，預設最新日期來排序
+            @RequestParam(defaultValue = "desc") String sort //  如果沒有傳參數，進行大到小的排序
+
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> ProductList = productService.getProducts(productQueryParams);
 //        List<Product> ProductList = productService.getProducts(category, search);

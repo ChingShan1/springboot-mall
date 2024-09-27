49d0f6ce72804e02502dac1b240d571fb7dde462
ChingShanHong <smmith818@gmail.com>
2024-09-27 17:45:37 +0800

    帳號功能-查詢訂單列表

--- springboot-mail/src/main/java/com/chingshan/springbootmail/controller/OrderController.java
+++ springboot-mail/src/main/java/com/chingshan/springbootmail/controller/OrderController.java
@@ -2,16 +2,19 @@ package com.chingshan.springbootmail.controller;
 
 
 import com.chingshan.springbootmail.dto.CreateOrderRequest;
+import com.chingshan.springbootmail.dto.OrderQueryParams;
 import com.chingshan.springbootmail.model.Order;
 import com.chingshan.springbootmail.service.OrderService;
+import com.chingshan.springbootmail.util.Page;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -29,4 +32,33 @@ public class OrderController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
+
+    @GetMapping("/users/{userId}/orders")
+    public ResponseEntity<Page<Order>> getOrders(
+            @PathVariable Integer userId,
+            @RequestParam(defaultValue = "10") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0)Integer offset
+    ){
+        OrderQueryParams orderQueryParams = new OrderQueryParams();
+        orderQueryParams.setUserId(userId);
+        orderQueryParams.setLimit(limit);
+        orderQueryParams.setOffset(offset);
+
+        List<Order> orderList = orderService.getOrder(orderQueryParams);
+
+        Integer count = orderService.countOrder(orderQueryParams);
+
+        Page<Order> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(count);
+        page.setResults(orderList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
+
+
+    }
+
+
+
 }
